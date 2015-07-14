5e7073f834bc6c33d5f301ee7dd00e8d9ce9c16d
Jamie Collinson <jamiecollinson@gmail.com>
2015-07-14 20:03:37 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 # install essentials
-sudo apt-get install software-properties-common bash-completion
+apt-get install software-properties-common bash-completion
 
 # install atom & popcorn-time
-sudo add-apt-repository ppa:webupd8team/atom ppa:webupd8team/popcorntime
-sudo apt-get update
-sudo apt-get install atom popcorn-time
+add-apt-repository ppa:webupd8team/atom ppa:webupd8team/popcorntime
+apt-get update
+apt-get install atom popcorn-time
