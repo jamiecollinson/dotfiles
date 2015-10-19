742be51d12b309b7568fab48b51a56a24e8b79b7
Jamie Collinson <jamiecollinson@gmail.com>
2015-10-19 07:55:59 +0100

    Add .config dir

---
+++ .config/awesome/debian/menu.lua
@@ -0,0 +1,142 @@
+-- automatically generated file. Do not edit (see /usr/share/doc/menu/html)
+
+module("debian.menu")
+
+Debian_menu = {}
+
+Debian_menu["Debian_Applications_Accessibility"] = {
+	{"xbindkeys","/usr/bin/xbindkeys"},
+	{"Xmag","xmag"},
+}
+Debian_menu["Debian_Applications_Editors"] = {
+	{"MousePad","/usr/bin/mousepad","/usr/share/pixmaps/mousepad.xpm"},
+	{"Xedit","xedit"},
+}
+Debian_menu["Debian_Applications_File_Management"] = {
+	{"Thunar","/usr/bin/thunar"},
+	{"Xarchiver","/usr/bin/xarchiver","/usr/share/pixmaps/xarchiver.xpm"},
+	{"Xfdesktop","xfdesktop"},
+}
+Debian_menu["Debian_Applications_Graphics"] = {
+	{"ImageMagick","/usr/bin/display.im6 logo:","/usr/share/pixmaps/display.im6.xpm"},
+	{"X Window Snapshot","xwd | xwud"},
+}
+Debian_menu["Debian_Applications_Network_Communication"] = {
+	{"Xbiff","xbiff"},
+}
+Debian_menu["Debian_Applications_Network_File_Transfer"] = {
+	{"Transmission BitTorrent Client (GTK)","/usr/bin/transmission-gtk","/usr/share/pixmaps/transmission.xpm"},
+}
+Debian_menu["Debian_Applications_Network_Web_Browsing"] = {
+	{"Netsurf Web Browser","/usr/bin/netsurf-gtk","/usr/share/pixmaps/netsurf.xpm"},
+}
+Debian_menu["Debian_Applications_Network"] = {
+	{ "Communication", Debian_menu["Debian_Applications_Network_Communication"] },
+	{ "File Transfer", Debian_menu["Debian_Applications_Network_File_Transfer"] },
+	{ "Web Browsing", Debian_menu["Debian_Applications_Network_Web_Browsing"] },
+}
+Debian_menu["Debian_Applications_Programming"] = {
+	{"Python (v2.7)", "x-terminal-emulator -e ".."/usr/bin/python2.7","/usr/share/pixmaps/python2.7.xpm"},
+	{"Python (v3.4)", "x-terminal-emulator -e ".."/usr/bin/python3.4","/usr/share/pixmaps/python3.4.xpm"},
+}
+Debian_menu["Debian_Applications_Science_Mathematics"] = {
+	{"Xcalc","xcalc"},
+}
+Debian_menu["Debian_Applications_Science"] = {
+	{ "Mathematics", Debian_menu["Debian_Applications_Science_Mathematics"] },
+}
+Debian_menu["Debian_Applications_Shells"] = {
+	{"Bash", "x-terminal-emulator -e ".."/bin/bash --login"},
+	{"Dash", "x-terminal-emulator -e ".."/bin/dash -i"},
+	{"Sh", "x-terminal-emulator -e ".."/bin/sh --login"},
+}
+Debian_menu["Debian_Applications_Sound"] = {
+	{"Xfce Mixer","/usr/bin/xfce4-mixer","/usr/share/pixmaps/xfce4-mixer.xpm"},
+}
+Debian_menu["Debian_Applications_System_Administration"] = {
+	{"Editres","editres"},
+	{"LXAppearance","/usr/bin/lxappearance"},
+	{"Xclipboard","xclipboard"},
+	{"Xfce Application Finder","xfce4-appfinder","/usr/share/pixmaps/xfce4-appfinder.xpm"},
+	{"Xfontsel","xfontsel"},
+	{"Xkill","xkill"},
+	{"Xrefresh","xrefresh"},
+}
+Debian_menu["Debian_Applications_System_Hardware"] = {
+	{"Xvidtune","xvidtune"},
+}
+Debian_menu["Debian_Applications_System_Monitoring"] = {
+	{"Pstree", "x-terminal-emulator -e ".."/usr/bin/pstree.x11","/usr/share/pixmaps/pstree16.xpm"},
+	{"Top", "x-terminal-emulator -e ".."/usr/bin/top"},
+	{"Xconsole","xconsole -file /dev/xconsole"},
+	{"Xev","x-terminal-emulator -e xev"},
+	{"Xload","xload"},
+}
+Debian_menu["Debian_Applications_System"] = {
+	{ "Administration", Debian_menu["Debian_Applications_System_Administration"] },
+	{ "Hardware", Debian_menu["Debian_Applications_System_Hardware"] },
+	{ "Monitoring", Debian_menu["Debian_Applications_System_Monitoring"] },
+}
+Debian_menu["Debian_Applications_Terminal_Emulators"] = {
+	{"X-Terminal as root (GKsu)","/usr/bin/gksu -u root /usr/bin/x-terminal-emulator","/usr/share/pixmaps/gksu-debian.xpm"},
+	{"Xfce Terminal","/usr/bin/xfce4-terminal","/usr/share/pixmaps/xfce4-terminal.xpm"},
+	{"XTerm","xterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+	{"XTerm (Unicode)","uxterm","/usr/share/pixmaps/xterm-color_32x32.xpm"},
+}
+Debian_menu["Debian_Applications_Video"] = {
+	{"VLC media player","/usr/bin/qvlc","/usr/share/icons/hicolor/32x32/apps/vlc.xpm"},
+}
+Debian_menu["Debian_Applications_Viewers"] = {
+	{"Xditview","xditview"},
+}
+Debian_menu["Debian_Applications"] = {
+	{ "Accessibility", Debian_menu["Debian_Applications_Accessibility"] },
+	{ "Editors", Debian_menu["Debian_Applications_Editors"] },
+	{ "File Management", Debian_menu["Debian_Applications_File_Management"] },
+	{ "Graphics", Debian_menu["Debian_Applications_Graphics"] },
+	{ "Network", Debian_menu["Debian_Applications_Network"] },
+	{ "Programming", Debian_menu["Debian_Applications_Programming"] },
+	{ "Science", Debian_menu["Debian_Applications_Science"] },
+	{ "Shells", Debian_menu["Debian_Applications_Shells"] },
+	{ "Sound", Debian_menu["Debian_Applications_Sound"] },
+	{ "System", Debian_menu["Debian_Applications_System"] },
+	{ "Terminal Emulators", Debian_menu["Debian_Applications_Terminal_Emulators"] },
+	{ "Video", Debian_menu["Debian_Applications_Video"] },
+	{ "Viewers", Debian_menu["Debian_Applications_Viewers"] },
+}
+Debian_menu["Debian_Games_Toys"] = {
+	{"Oclock","oclock"},
+	{"Xclock (analog)","xclock -analog"},
+	{"Xclock (digital)","xclock -digital -update 1"},
+	{"Xeyes","xeyes"},
+	{"Xlogo","xlogo"},
+}
+Debian_menu["Debian_Games"] = {
+	{ "Toys", Debian_menu["Debian_Games_Toys"] },
+}
+Debian_menu["Debian_Help"] = {
+	{"Xman","xman"},
+}
+Debian_menu["Debian_Screen_Locking"] = {
+	{"Lock Screen (XScreenSaver)","/usr/bin/xscreensaver-command -lock"},
+}
+Debian_menu["Debian_Screen_Saving"] = {
+	{"Activate ScreenSaver (Next)","/usr/bin/xscreensaver-command -next"},
+	{"Activate ScreenSaver (Previous)","/usr/bin/xscreensaver-command -prev"},
+	{"Activate ScreenSaver (Random)","/usr/bin/xscreensaver-command -activate"},
+	{"Demo Screen Hacks","/usr/bin/xscreensaver-command -demo"},
+	{"Disable XScreenSaver","/usr/bin/xscreensaver-command -exit"},
+	{"Enable XScreenSaver","/usr/bin/xscreensaver"},
+	{"Reinitialize XScreenSaver","/usr/bin/xscreensaver-command -restart"},
+	{"ScreenSaver Preferences","/usr/bin/xscreensaver-command -prefs"},
+}
+Debian_menu["Debian_Screen"] = {
+	{ "Locking", Debian_menu["Debian_Screen_Locking"] },
+	{ "Saving", Debian_menu["Debian_Screen_Saving"] },
+}
+Debian_menu["Debian"] = {
+	{ "Applications", Debian_menu["Debian_Applications"] },
+	{ "Games", Debian_menu["Debian_Games"] },
+	{ "Help", Debian_menu["Debian_Help"] },
+	{ "Screen", Debian_menu["Debian_Screen"] },
+}
