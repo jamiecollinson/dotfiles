2c98a1cc856c16288fabaf9eb59e6f40269358eb
Jamie Collinson <jamiecollinson@gmail.com>
2015-10-28 09:11:14 +0000

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -9,3 +9,7 @@ add-apt-repository ppa:webupd8team/atom
 add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
 apt-get install atom neovim
+
+# install vim-plug
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
