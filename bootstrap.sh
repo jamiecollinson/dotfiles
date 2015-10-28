236103361519d7cd50a2c2268cd194ebeced5796
Jamie Collinson <jamiecollinson@gmail.com>
2015-10-28 09:09:38 +0000

    Add youcompleteme deps and neovim to bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,8 +1,11 @@
-# install essentials
-apt-get install software-properties-common bash-completion curl tmux dmenu
+#!/bin/bash
 
-# install atom & popcorn-time
+# install essentials
+apt-get install software-properties-common bash-completion curl tmux dmenu \
+  build-essential cmake
+
+# install external ppas
 add-apt-repository ppa:webupd8team/atom
-add-apt-repository ppa:webupd8team/popcorntime
+add-apt-repository ppa:neovim-ppa/unstable
 apt-get update
-apt-get install atom popcorn-time
+apt-get install atom neovim
