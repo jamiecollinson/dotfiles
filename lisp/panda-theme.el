8cbf7fefac7af6d52d66e4cdffce400d3e731e94
Jamie Collinson <jamiecollinson@gmail.com>
2018-01-13 21:14:37 +0000

    Working on panda theme

--- lisp/panda-theme.el
+++ lisp/panda-theme.el
@@ -1,12 +1,13 @@
 (deftheme panda
   "Created 2018-01-11.")
 
-(let ((bg "#2e2e2e")
-      (bg+1 "#383838")
-      (bg+2 "#424242")
-      (bg+3 "#474747")
-      (fg "#b6b6b6")
-      (fg+1 "#e6e6e6")
+(let ((bg "#292A2B")
+
+      (fg "#E6E6E6")
+
+      (caret "#F8F8F0")
+      (comment "#676B79")
+      (hl "#404954")
 
       (cyan "#35ffdc")
       (pink "#ff90d0")
@@ -17,19 +18,61 @@
 
   (custom-theme-set-faces
    'panda
+
+   ;; Default
    `(default ((t (:background ,bg :foreground ,fg))))
-   `(cursor ((t (:background ,bg+3))))
-   `(hl-line ((t (:background ,bg+3))))
-   `(font-lock-keyword-face ((t (:foreground ,pink))))
-   `(font-lock-doc-face ((t (:foreground ,orange))))
-   `(font-lock-string-face ((t (:foreground ,orange))))
+   `(italic ((t (:italic t :font "Operator Mono Italic 12"))))
+   `(cursor ((t (:background ,caret))))
+   `(ffap ((t :foreground ,caret)))
+   `(fringe ((t (:background ,bg))))
+   `(highlight ((t (:background ,hl))))
+   `(region ((t :background ,hl)))
+   `(linum ((t :foreground ,comment)))
+   `(lazy-highlight ((t (:background ,orange))))
+   `(link ((t (:foreground ,blue :underline t))))
+   `(minibuffer-prompt ((t :foreground ,pink)))
+   `(region ((t (:background ,orange :foreground ,bg))))
+   `(show-paren-match-face ((t (:background ,red))))
+   `(trailing-whitespace ((t :foreground nil :background ,red)))
+   `(vertical-border ((t (:foreground ,comment))))
+   `(warning ((t (:foreground ,red))))
+
+   ;; Fontlock faces
    `(font-lock-builtin-face ((t (:foreground ,blue))))
-   `(font-lock-type-face ((t (:foreground ,cyan)))))
+   `(font-lock-comment-face ((t (:foreground ,comment :inherit italic))))
+   `(font-lock-constant-face ((t (:foreground ,orange))))
+   `(font-lock-doc-face ((t (:foreground ,comment :inherit italic))))
+   `(font-lock-function-name-face ((t (:foreground ,orange))))
+   `(font-lock-keyword-face ((t (:foreground ,pink))))
+   `(font-lock-string-face ((t (:foreground ,cyan))))
+   `(font-lock-type-face ((t (:foreground ,cyan))))
+   `(font-lock-variable-name-face ((t (:foreground ,orange))))
+   `(font-lock-warning-face ((t (:foreground ,red :background ,hl))))
+
+   ;; rainbow-delimiters
+   `(rainbow-delimiters-depth-1-face ((t :foreground ,caret)))
+   `(rainbow-delimiters-depth-2-face ((t :foreground ,cyan)))
+   `(rainbow-delimiters-depth-3-face ((t :foreground ,pink)))
+   `(rainbow-delimiters-depth-4-face ((t :foreground ,blue)))
+   `(rainbow-delimiters-depth-5-face ((t :foreground ,orange)))
+   `(rainbow-delimiters-depth-6-face ((t :foreground ,purple)))
+   `(rainbow-delimiters-depth-7-face ((t :foreground ,cyan)))
+   `(rainbow-delimiters-depth-8-face ((t :foreground ,pink)))
+   `(rainbow-delimiters-unmatched-face ((t :foreground ,red :underline ,red)))
+
+   ;; git-gutter
+   `(git-gutter:added ((t :foreground ,cyan)))
+   `(git-gutter:changed ((t :foreground ,orange)))
+   `(git-gutter:deleted ((t :foreground ,red)))
+
+  ;; RJSX mode
+   `(rjsx-attr ((t :foreground ,orange :inherit italic)))
+   `(rjsx-tag ((t :foreground ,red)))
+   )
 
   (custom-theme-set-variables
    'panda
    `(ansi-color-names-vector
-     [,bg ,red ,purple ,orange ,blue ,pink ,cyan ,fg]))
-  )
+     [,bg ,red ,purple ,orange ,blue ,pink ,cyan ,fg])))
 
 (provide-theme 'panda)
